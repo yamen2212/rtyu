308d06894161a1bdba8cad4b7e1e9d13264a6dea
yamen2212 <karmahossam112@gmail.com>
2023-11-11 18:48:11 +0200

    commit to main

---
+++ script.js
@@ -0,0 +1,26 @@
+// var copyText=document.getElementByld(" mylnput");
+// copyText.addEventListener ("click", function() {
+// copyText.select();
+// copyText.setSelectionRange(0, 99999);
+// navigator.clipboard.writeText( copyText.value);
+// alert ("Copied the text: " + copyText.value);})
+// var copyText1=document.getElementByld("inp2");
+
+
+// copyText1.addEventListener ("click", function() {
+// copyText1.select();
+// copyText1.setSelectionRange(0
+// , 99999)
+// navigator.clipboard.writeText( copyText1.value);
+// alert("Copied the text: "+ copyText1.value);})
+// var copyText2=document.getElementByld("iпр3");
+// copyText2.addEventListener("click", function () {
+// copyText.select ();
+// copyText2.setSelectionRange(0, 99999);
+// navigator.clipboard.writeText (copyText.value);
+// alert ("Copied the text: "+ copyText2.value);})
+
+
+
+
+
